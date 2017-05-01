7825cee31139eae1b404c80e252fd0e4ffacd4f7
cps274203 <cps274203@gnail.com>
2017-05-01 17:38:35 +0530

    makeit better

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -5,12 +5,15 @@ admin.autodiscover()
 
 import hello.views
 
+handler404 = 'hello.views.handler404'
+handler500 = 'hello.views.handler500'
 # Examples:
 # url(r'^$', 'gettingstarted.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
     url(r'^$', hello.views.index, name='index'),
+    url(r'call.html', hello.views.call, name='call'),
     url(r'^db', hello.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+from django.shortcuts import render_to_response
 
 from .models import Greeting
 
@@ -8,6 +10,9 @@ def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, 'index.html')
 
+def call(request):
+    # return HttpResponse('Hello from Python!')
+    return render(request, 'call.html')
 
 def db(request):
 
@@ -18,3 +23,17 @@ def db(request):
 
     return render(request, 'db.html', {'greetings': greetings})
 
+def handler404(request):
+    return render(request, '404.html')
+    #response = render_to_response('404.html', {},
+    #                          context_instance=RequestContext(request))
+    #response.status_code = 404
+    #return response
+
+
+def handler500(request):
+    return render(request, '500.html')
+    #response = render_to_response('500.html', {},
+    #                          context_instance=RequestContext(request))
+    #response.status_code = 500
+    #return response
